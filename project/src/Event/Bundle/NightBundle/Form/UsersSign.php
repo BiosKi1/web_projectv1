e0dfb0c605098723c29dfed32185bdfb2af0a9e2
francis <francis@test.fr>
2016-12-05 11:19:14 +0100

    form user sign

---
+++ project/src/Event/Bundle/NightBundle/Form/UsersSign.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Event\Bundle\NightBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class UsersSign extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('mdp')
+            ->add('adresse')
+            ->add('ville')
+            ->add('cp')
+            ->add('mail', EmailType::class)
+            ->add('genre', ChoiceType::class, array(
+                'choices' => array(
+                    'Shônen' => 'Shônen',
+                    'Shôjo' => 'Shôjo',
+                    'Hentai' => 'Hentai',
+                    'Dôjinshi ' => 'Dôjinshi ',
+                    'Yaoi  / Yuri ' => 'Yaoi',
+                    'Shônen-ai' => 'Shônen-ai',
+                    'Seinen' => 'Seinen')))
+            ->add('couverture',FileType::class)
+        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Event\Bundle\NightBundle\Entity\Users'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'event_bundle_nightbundle_users';
+    }
+
+
+}
